df91e17aef7bb6d01199d2f0855838390c72337f
anarchyroot <matt@shotbymatt.com>
2017-06-13 12:43:17 +0100

    migrate

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def say_hello(request):
     return render(request, 'index.html')
+
+
