76bc8b4e031cbed07e504fc06905c5cef9a7bb45
anarchyroot <matt@shotbymatt.com>
2017-06-13 15:52:24 +0100

    add email field to contacts

--- hello/models.py
+++ hello/models.py
@@ -8,6 +8,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     mobile = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
