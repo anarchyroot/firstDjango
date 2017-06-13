ab56d3e94bea0388591ea3f5c889ae0a4e7d4845
anarchyroot <matt@shotbymatt.com>
2017-06-13 16:22:03 +0100

    add image to models

--- hello/models.py
+++ hello/models.py
@@ -9,6 +9,7 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=255)
     mobile = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/', default='images/no-img.jpg')
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
