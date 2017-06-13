b2f5eaf10ca08ec6000fb677632d1eee45e1e399
anarchyroot <matt@shotbymatt.com>
2017-06-13 14:34:40 +0100

    models firstname lastname

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class Contact(models.Model):
 
-    class Meta: # include this to ensure build in IDE
+class Contact(models.Model):
+    class Meta:  # include this to ensure build in IDE
         app_label = "hello"
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     mobile = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
